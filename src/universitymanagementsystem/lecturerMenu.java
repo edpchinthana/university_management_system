1c78c837434afd4fd673348346b8e9046b1b016c
edpchinthana <edpchinthana@outlook.com>
2019-12-26 01:26:07 +0530

    lecturerCourses.java is created

--- src/universitymanagementsystem/lecturerMenu.java
+++ src/universitymanagementsystem/lecturerMenu.java
@@ -68,6 +68,11 @@ public class lecturerMenu extends javax.swing.JFrame {
 
         jButton2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton2.setText("Courses");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton1.setText("Edit Info");
@@ -166,6 +171,10 @@ public class lecturerMenu extends javax.swing.JFrame {
         lecturerEditInfo lefObject = new lecturerEditInfo(username, name, telephone);
         lefObject.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
